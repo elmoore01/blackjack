3c8dc85cb43f9330fa9fc37f3f0f58984f2f4289
Eric Moore <eric@educate-ca.org>
2020-08-11 14:53:37 -0700

    second commit

--- js/main.js
+++ js/main.js
@@ -2,214 +2,121 @@
 
 const suits = ["spades", "diamonds", "clubs", "hearts"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let deck = new Array();
 
-const blkjkLookup = {
-    spds: {
-        A: {
-            imgUrl: '../images/spades/spades-A.svg'
-        },
-        K: {
-            imgUrl: '../images/spades/spades-K.svg'
-        },
-        Q: {
-            imgUrl: '../images/spades/spades-Q.svg'
-        },
-        J: {
-            imgUrl: '../images/spades/spades-J.svg'
-        },
-        2: {
-            imgUrl: '../images/spades/spades-r02.svg'
-        },
-        3: {
-            imgUrl: '../images/spades/spades-r03.svg'
-        },
-        4: {
-            imgUrl: '../images/spades/spades-r04.svg'
-        },
-        5: {
-            imgUrl: '../images/spades/spades-r05.svg'
-        },
-        6:  {
-            imgUrl: '../images/spades/spades-r06.svg'
-        },
-        7:  {
-            imgUrl: '../images/spades/spades-r07.svg'
-        },
-        8:  {
-            imgUrl: '../images/spades/spades-r08.svg'
-        },
-        9:  {
-            imgUrl: '../images/spades/spades-r09.svg'
-        },
-        10: {
-            imgUrl: '../images/spades/spades-r10.svg'
-        },
-
-    clbs: {
-        A: {
-            imgUrl: '../images/clubs/clubs-A.svg'
-        },
-        K: {
-            imgUrl: '../images/clubs/clubs-K.svg'
-        },
-        Q: {
-            imgUrl: '../images/clubs/clubs-Q.svg'
-        },
-        J: {
-            imgUrl: '../images/clubs/clubs-J.svg'
-        },
-        2: {
-            imgUrl: '../images/clubs/clubs-r02.svg'
-        },
-        3: {
-            imgUrl: '../images/clubs/clubs-r03.svg'
-        },
-        4: {
-            imgUrl: '../images/clubs/clubs-r04.svg'
-        },
-        5: {
-            imgUrl: '../images/clubs/clubs-r05.svg'
-        },
-        6:  {
-            imgUrl: '../images/clubs/clubs-r06.svg'
-        },
-        7:  {
-            imgUrl: '../images/clubs/clubs-r07.svg'
-        },
-        8:  {
-            imgUrl: '../images/clubs/clubs-r08.svg'
-        },
-        9:  {
-            imgUrl: '../images/clubs/clubs-r09.svg'
-        },
-        10: {
-            imgUrl: '../images/clubs/clubs-r10.svg'
-        },
-    
-    hrts: {
-        A: {
-            imgUrl: '../images/hearts/hearts-A.svg'
-        },
-        K: {
-            imgUrl: '../images/hearts/hearts-K.svg'
-        },
-        Q: {
-            imgUrl: '../images/hearts/hearts-Q.svg'
-        },
-        J: {
-            imgUrl: '../images/hearts/hearts-r09.svg'
-        },
-        2: {
-            imgUrl: '../images/hearts/hearts-r02.svg'
-        },
-        3: {
-            imgUrl: '../images/hearts/hearts-r03.svg'
-        },
-        4: {
-            imgUrl: '../images/hearts/hearts-r04.svg'
-        },
-        5: {
-            imgUrl: '../images/hearts/hearts-r05.svg'
-        },
-        6:  {
-            imgUrl: '../images/hearts/hearts-r06.svg'
-        },
-        7:  {
-            imgUrl: '../images/hearts/hearts-r07.svg'
-        },
-        8:  {
-            imgUrl: '../images/hearts/hearts-r08.svg'
-        },
-        9:  {
-            imgUrl: '../images/hearts/hearts-r09.svg'
-        },
-        10: {
-            imgUrl: '../images/hearts/hearts-r10.svg'
-        },
-
-    dmds: {
-        A: {
-            imgUrl: '../images/diamonds/diamonds-A.svg'
-        },
-        K: {
-            imgUrl: '../images/diamonds/diamonds-K.svg'
-        },
-        Q: {
-            imgUrl: '../images/diamonds/diamonds-Q.svg'
-        },
-        J: {
-            imgUrl: '../images/diamonds/diamonds-J.svg'
-        },
-        2: {
-            imgUrl: '../images/diamonds/diamonds-r02.svg'
-        },
-        3: {
-            imgUrl: '../images/diamonds/diamonds-r03.svg'
-        },
-        4: {
-            imgUrl: '../images/diamonds/diamonds-r04.svg'
-        },
-        5: {
-            imgUrl: '../images/diamonds/diamonds-r05.svg'
-        },
-        6:  {
-            imgUrl: '../images/diamonds/diamonds-r06.svg'
-        },
-        7:  {
-            imgUrl: '../images/diamonds/diamonds-r07.svg'
-        },
-        8:  {
-            imgUrl: '../images/diamonds/diamonds-r08.svg'
-        },
-        9:  {
-            imgUrl: '../images/diamonds/diamonds-r09.svg'
-        },
-        10: {
-            imgUrl: '../images/diamonds/diamonds-r10.svg'
-        },
-    }
-
-
-
-// console.log(blkjkLookup.spds.A.imgUrl)
-
+let running = false;
+let blackjack = false;
+let deal;
+let player = [];
+let dealer = [];
 /*----- app's state (variables) -----*/
 
-let deck = new Array();
-let playerScore;
-let dealerScore;
-let playerWinnings;
-let turn;
-// let currentBet;
-let results;
-let winner;
+let hand = [];
+let wager = 0;
+let cash = 1000;
+let bank = 0;
+let element = '';
+let playerScore = 0;
+let dealerScore = 0;
+let gameInPlay = true;
 
 /*----- cached element references -----*/
 
-const scoreElements = {
-    player: document.querySelector('#player-score'),
-    computer: document.querySelector('#computer-score')
-}
 
 /*----- event listeners -----*/
 
+document.getElementById('hit').addEventListener('click', hit);
+document.getElementById('stand').addEventListener('click', stay);
 
 /*----- functions -----*/
 
-function createDeck()
-    {
-        deck = new Array();
-        for (var i = 0 ; i < values.length; i++)
-        {
-            for(var x = 0; x < suits.length; x++)
-            {
-                var weight = parseInt(values[i]);
-                if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
-                    weight = 10;
-                if (values[i] == "A")
-                    weight = 11;
-                var card = { Value: values[i], Suit: suits[x], Weight: weight };
-                deck.push(card);
-            }
+function createDeck() {
+    deck = new Array();
+    for (let i = 0; i < values.length; i++) {
+        for (let x = 0; x < suits.length; x++) {
+            let weight = parseInt(values[i]);
+            if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                weight = 10;
+            if (values[i] == "A")
+                weight = 11;
+            let card = { Value: values[i], Suit: suits[x], Weight: weight };
+            deck.push(card);
         }
     }
+}
+
+function shuffle() {
+    for (let i = 0; i < 1000; i++) {
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random() * deck.length));
+        let tmp = deck[location1];
+
+        deck[location1] = deck[location2];
+        deck[location2] = tmp;
+    }
+}
+
+function dealToDealer() {
+
+    dealer.push(deck.pop())
+}
+
+function dealToPlayer() {
+    player.push(deck.pop())
+}
+
+function init() {
+    createDeck();
+    shuffle();
+    dealToPlayer();
+    dealToPlayer();
+    dealToDealer();
+    dealToDealer();
+    render();
+}
+
+init();
+
+function checkScore() {
+    playerScore=0, dealerScore=0;
+    for (let i = 0; i < player.length; i++) {
+        playerScore += player[i].Weight;
+    }
+    for (let i = 0; i < dealer.length; i++) {
+        dealerScore += dealer[i].Weight;
+    }
+}
+
+function render() {
+    checkScore();
+    if(gameInPlay==false) {
+        checkWinner();
+    }
+}
+
+function hit() {
+    dealToPlayer();
+    render();
+}
+
+function stay() {
+    while (dealerScore < 15) {
+        dealToDealer();
+        checkScore();
+    }
+    gameInPlay=false;
+
+    render();
+}
+
+function checkWinner() {
+    if(playerScore > dealerScore) {
+        console.log('player wins')
+    }
+    else if (dealerScore > playerScore) {
+        console.log('dealer wins')
+    }
+    else {
+        console.log('tie')
+    }
+}
